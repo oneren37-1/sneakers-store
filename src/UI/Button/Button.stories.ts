6835a31eeecb9482ed5c593eeda9a88592d16766
oneren37 <oneren38@gmail.com>
2024-01-27 14:42:03 +0300

    button ui component

---
+++ src/UI/Button/Button.stories.ts
@@ -0,0 +1,42 @@
+import { StoryFn, Meta } from '@storybook/vue3';
+import Button from './Button.vue';
+import Icon from "../Icon/Icon.vue";
+
+export default {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+        block: {
+            control: { type: 'boolean' },
+            defaultValue: true,
+        }
+    },
+} as Meta<typeof Button>;
+
+export const DefaultButton: StoryFn<typeof Button> = (args) => ({
+    components: { Button },
+    setup() { return { args }; },
+    template: '<Button v-bind="args">Button</Button>',
+});
+
+export const LefIconButton: StoryFn<typeof Button> = (args) => ({
+    components: { Button, Icon },
+    setup() { return { args }; },
+    template: `<Button v-bind="args">
+      <template #left-icon>
+        <Icon name="arrowLeft" />
+      </template>
+      Button
+    </Button>`,
+});
+
+export const RightIconButton: StoryFn<typeof Button> = (args) => ({
+    components: { Button, Icon },
+    setup() { return { args }; },
+    template: `<Button v-bind="args">
+      Button
+      <template #right-icon>
+        <Icon name="arrowRight"/>
+      </template>
+    </Button>`,
+});
---
+++ src/UI/Button/Button.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  block: {
+    type: Boolean,
+    default: false,
+  }
+});
+</script>
+
+<template>
+  <button class="button" :class="[props.block ? 'block' : '']">
+    <span v-if="$slots['left-icon']" class="left-icon">
+      <slot name="left-icon"/>
+    </span>
+    <slot />
+    <span v-if="$slots['right-icon']" class="right-icon">
+      <slot name="right-icon"/>
+    </span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+  .button {
+    outline: none;
+    border: none;
+    padding: 18px 31px;
+
+    border-radius: 18px;
+    background: #9DD458;
+
+    color: #FFF;
+    text-align: center;
+    font-size: 16px;
+    line-height: 19px;
+    font-style: normal;
+    font-weight: 600;
+
+    display: flex;
+    align-items: center;
+
+    cursor: pointer;
+  }
+
+  .left-icon {
+    padding-right: 10px;
+  }
+
+  .right-icon {
+    padding-left: 10px;
+  }
+
+  .left-icon,
+  .right-icon {
+    width: 14px;
+    height: 12px;
+
+    display: flex;
+    align-items: center;
+  }
+</style>
